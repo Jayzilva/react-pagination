2140779299ee033bb0940db79e53945a5e822bbe
FIRST_NAME LAST_NAME <116673159+Jayzilva@users.noreply.github.com>
2023-03-14 12:47:43 +0530

    up

--- complain-sc/thirdweb-app/pages/index.js
+++ complain-sc/thirdweb-app/pages/index.js
@@ -7,8 +7,19 @@ export default function Home() {
   const contractAdd ="0xD6D55c31D6b917D692A253eEf668448944eE9453";
   const { contract } = useContract(contractAdd);
   //const { data: officer } = useContractRead(contract, "officer");
-  //const { data: nextId } = useContractRead(contract, "nextId")
+  const { data: nextId } = useContractRead(contract, "nextId")
+  const [rId, setRId] = useState(0);
+  const [aRemark, setARemark] = useState("");
+  const [rRemark, setRRemark] = useState("");
   const { mutateAsync: fileComplaint } = useContractWrite(contract, "fileComplaint");
+  const { data: pendingApprovals } = useContractRead(contract, "pendingApprovals", 0)
+  const { data: pendingResolutions } = useContractRead(contract, "pendingResolutions", 0)
+  const { mutateAsync: calcPendingApprovalIds } = useContractWrite(contract, "calcPendingApprovalIds")
+  const { mutateAsync: calcPendingResolutionIds } = useContractWrite(contract, "calcPendingResolutionIds")
+
+  const { mutateAsync: approveComplaint } = useContractWrite(contract, "approveComplaint")
+  const { mutateAsync: resolveComplaint } = useContractWrite(contract, "resolveComplaint")
+
 
   const [id, setId] = useState(0);
   const [title, setTitle] = useState("");
@@ -29,6 +40,25 @@ export default function Home() {
         console.error("contract call failure", err);
     }
 }
+
+const getPendingApprovals = async () => {
+  try {
+      const data = await calcPendingApprovalIds([]);
+      console.info("contract call successs", data);
+  } catch (err) {
+      console.error("contract call failure", err);
+  }
+}
+
+const handleApproveComplaint = async () => {
+  try {
+      const data = await approveComplaint([id, aRemark]);
+      console.info("contract call successs", data);
+  } catch (err) {
+      console.error("contract call failure", err);
+  }
+}
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
@@ -57,6 +87,26 @@ export default function Home() {
                 </div>
             )}
           
+          <hr/>
+          <div> 
+          <p >Pending Approvals</p>
+          <button  onClick={getPendingApprovals}>Next Pending Approval ID</button>
+          {
+              pendingApprovals && (
+              <p >: {pendingApprovals.toString()}</p>
+              )
+          }
+          </div>
+
+          <hr/>
+          <div>
+          <p>Complaint Id: </p>
+                <input type="number" placeholder='Enter Id Here' onChange={(e) => { setId(e.target.value) }} />
+          <p>Your Remark: </p>
+                <input type="text" placeholder='Enter Remark Here' onChange={(e) => { setARemark(e.target.value) }} />
+          
+                <button  onClick={handleApproveComplaint}>Approve Complaint</button>
+          </div>
       </main>
     </div>
   );
